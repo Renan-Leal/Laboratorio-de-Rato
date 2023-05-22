b48b8a2b5560af1ab268f4a78107b9a31062d2b8
Renan Duarte Leal <104607462+Renan-Leal@users.noreply.github.com>
2023-05-22 15:48:31 -0300

    Atualização da classe Treino e da tabela de treino no script SQL;

--- project/model/vo/Treino.java
+++ project/model/vo/Treino.java
@@ -3,99 +3,49 @@ package model.vo;
 public class Treino {
 	private Integer id;
 	private Usuario cliente;
-	private String A;
-	private String B;
-	private String C;
-	private String D;
-	private String E;
-	
+	private String treino;
 	
 	public Treino() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-
-
-	public Treino(Integer id, Usuario cliente, String a, String b, String c, String d, String e) {
+	
+	public Treino(Integer id, Usuario cliente, String treino) {
 		super();
 		this.id = id;
 		this.cliente = cliente;
-		A = a;
-		B = b;
-		C = c;
-		D = d;
-		E = e;
+		this.treino = treino;
 	}
-
 
 	public Integer getId() {
 		return id;
 	}
 
-
 	public void setId(Integer id) {
 		this.id = id;
 	}
-
 
 	public Usuario getCliente() {
 		return cliente;
 	}
 
-
 	public void setCliente(Usuario cliente) {
 		this.cliente = cliente;
 	}
 
-
-	public String getA() {
-		return A;
+	public String getTreino() {
+		return treino;
 	}
 
-
-	public void setA(String a) {
-		A = a;
+	public void setTreino(String treino) {
+		this.treino = treino;
 	}
+	
+	
 
-
-	public String getB() {
-		return B;
-	}
-
-
-	public void setB(String b) {
-		B = b;
-	}
-
-
-	public String getC() {
-		return C;
-	}
-
-
-	public void setC(String c) {
-		C = c;
-	}
-
-
-	public String getD() {
-		return D;
-	}
-
-
-	public void setD(String d) {
-		D = d;
-	}
-
-
-	public String getE() {
-		return E;
-	}
-
-
-	public void setE(String e) {
-		E = e;
-	}
+	
+	
+	
 	
 	
 	
