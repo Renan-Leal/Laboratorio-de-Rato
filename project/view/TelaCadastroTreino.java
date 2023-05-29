12bfaf44c30cd7cd2a37a8d0623158e8c47c0ad5
Renan Duarte Leal <104607462+Renan-Leal@users.noreply.github.com>
2023-05-29 14:03:35 -0300

    Codificação da tela de cadastro de Treino;

--- project/view/TelaCadastroTreino.java
+++ project/view/TelaCadastroTreino.java
@@ -1,13 +1,25 @@
 package view;
 
 import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JTextField;
+import javax.swing.JTextPane;
+import model.vo.NivelTreino;
 
 public class TelaCadastroTreino {
 
-	private JFrame frame;
+	private JFrame frmCadastroDeTreino;
+	private JLabel lblPrazo;
+	private JLabel lblNvl;
+	private JLabel lblTreino;
+	private JTextField txtPrazo;
+	private JComboBox cbNvl;
+	private JTextPane txtTreino;
+
+	private JButton btnCadastrar;
 
 	/**
 	 * Launch the application.
@@ -17,7 +29,7 @@ public class TelaCadastroTreino {
 			public void run() {
 				try {
 					TelaCadastroTreino window = new TelaCadastroTreino();
-					window.frame.setVisible(true);
+					window.frmCadastroDeTreino.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -36,14 +48,39 @@ public class TelaCadastroTreino {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
+		frmCadastroDeTreino = new JFrame();
+		frmCadastroDeTreino.setTitle("Cadastro de Treino");
+		frmCadastroDeTreino.setBounds(100, 100, 364, 606);
+		frmCadastroDeTreino.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmCadastroDeTreino.getContentPane().setLayout(null);
 		
-		JLabel lblCliente = new JLabel("Per");
-		lblCliente.setBounds(28, 23, 46, 14);
-		frame.getContentPane().add(lblCliente);
+		lblPrazo = new JLabel("Prazo:");
+		lblPrazo.setBounds(37, 26, 46, 14);
+		frmCadastroDeTreino.getContentPane().add(lblPrazo);
+		
+		lblNvl = new JLabel("Nível:");
+		lblNvl.setBounds(188, 26, 46, 14);
+		frmCadastroDeTreino.getContentPane().add(lblNvl);
+		
+		cbNvl = new JComboBox(new String[] {"BÁSICO","INTERMEDIÁRIO","AVANÇADO"});
+		cbNvl.setBounds(224, 22, 82, 22);
+		frmCadastroDeTreino.getContentPane().add(cbNvl);
+		
+		txtPrazo = new JTextField();
+		txtPrazo.setBounds(77, 23, 86, 20);
+		frmCadastroDeTreino.getContentPane().add(txtPrazo);
+		txtPrazo.setColumns(10);
+		
+		txtTreino = new JTextPane();
+		txtTreino.setBounds(37, 81, 269, 440);
+		frmCadastroDeTreino.getContentPane().add(txtTreino);
+		
+		lblTreino = new JLabel("Treino:");
+		lblTreino.setBounds(146, 56, 46, 14);
+		frmCadastroDeTreino.getContentPane().add(lblTreino);
+		
+		btnCadastrar = new JButton("Cadastrar");
+		btnCadastrar.setBounds(124, 533, 89, 23);
+		frmCadastroDeTreino.getContentPane().add(btnCadastrar);
 	}
-
 }
