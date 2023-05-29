ec02ba6fb8bec840614a9ed63f160bd677ae8d69
Renan Duarte Leal <104607462+Renan-Leal@users.noreply.github.com>
2023-05-29 14:03:03 -0300

    Alteração na classe Treino. Adição de atributos;

--- project/model/vo/Treino.java
+++ project/model/vo/Treino.java
@@ -4,18 +4,25 @@ public class Treino {
 	private Integer id;
 	private Usuario cliente;
 	private Usuario profissional;
+	private String dtCadastro;
+	private String dtTermino;
+	private  NivelTreino nivelTreino;
 	private String treino;
-	
+
 	public Treino() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 
-	public Treino(Integer id, Usuario cliente, Usuario profissional, String treino) {
+	public Treino(Integer id, Usuario cliente, Usuario profissional, String dtCadastro, String dtTermino,
+			NivelTreino nivelTreino, String treino) {
 		super();
 		this.id = id;
 		this.cliente = cliente;
 		this.profissional = profissional;
+		this.dtCadastro = dtCadastro;
+		this.dtTermino = dtTermino;
+		this.nivelTreino = nivelTreino;
 		this.treino = treino;
 	}
 
@@ -43,6 +50,30 @@ public class Treino {
 		this.profissional = profissional;
 	}
 
+	public String getDtCadastro() {
+		return dtCadastro;
+	}
+
+	public void setDtCadastro(String dtCadastro) {
+		this.dtCadastro = dtCadastro;
+	}
+
+	public String getDtTermino() {
+		return dtTermino;
+	}
+
+	public void setDtTermino(String dtTermino) {
+		this.dtTermino = dtTermino;
+	}
+
+	public NivelTreino getNivelTreino() {
+		return nivelTreino;
+	}
+
+	public void setNivelTreino(NivelTreino nivelTreino) {
+		this.nivelTreino = nivelTreino;
+	}
+
 	public String getTreino() {
 		return treino;
 	}
@@ -50,9 +81,7 @@ public class Treino {
 	public void setTreino(String treino) {
 		this.treino = treino;
 	}
-	
-	
-	
+
 	
 	
 	
